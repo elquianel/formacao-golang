64364b37ca8e61b8567f00b00eba4b3d35f6f39c
Elquiane Lima <elquianelima23@gmail.com>
2024-09-16 15:41:11 -0300

    Aprendendo sobre variaveis e alguns tipos

--- curso-01/hello.go
+++ curso-01/hello.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	//em go, você pode usar esse := como operador de atribuição para
+	//inferir o tipo e dizer implicitamente que isso é uma variável
+	//muito usado (declaração curta de variaveis)
+	nome := "Elquiane"
+	var versao float32 = 1.1
+
+	fmt.Println("Hello world,", nome)
+	fmt.Println("Esse programa está na versão: ", versao)
+
+	// fmt.Println("O tipo da variavel nome é:", reflect.TypeOf(nome))
+	fmt.Println("1- Iniciar monitoramento")
+	fmt.Println("2- Exibir logs")
+	fmt.Println("0- Sair do programa")
+
+	var comando int
+	//captura o input do usuario
+	//também diz que variavel vai armazenar o valor que recebe e seu tipo
+	// %d -> representa um inteiro -> é o modificador
+	//& -> endereço da variavel comando -> ponteiro
+	fmt.Scanf("%d", &comando)
+	//função da qual ele já identifica o tipo da variavel
+	// fmt.Scan(&comando)
+
+	fmt.Println("O ponteiro da minha variavel comando é:", &comando)
+	fmt.Println("O comando escolhido foi:", comando)
 }
